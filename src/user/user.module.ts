ac77cb162f924ebf8f11f058e608c813279f44a4
atapasha <atapashazade@gmail.com>
2026-06-25 18:45:36 +0330

    jwt added

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -4,9 +4,12 @@ import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, userSchema } from './user.schema';
 
+
+//For Admin
 @Module({
     imports: [MongooseModule.forFeature([{ name: User.name, schema:userSchema }])],
-  controllers: [UserController],
+ 
+    controllers: [UserController],
   providers: [UserService],
 })
 export class UserModule {}
