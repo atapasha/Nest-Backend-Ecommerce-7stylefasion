28d4c08e8c7bece4cbe65a3a4a7ffa4313b5b57b
atapasha <atapashazade@gmail.com>
2026-06-25 12:05:21 +0330

    db files added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './user/user.module';
   
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest/27017/ecommerce')],
+  imports: [MongooseModule.forRoot('mongodb://localhost/27017/ecommerce'), UserModule],
   controllers: [],
   providers: [],
 })
