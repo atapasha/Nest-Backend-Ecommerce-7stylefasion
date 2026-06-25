d14b2d1f8a4743b91c5c122d9c994d22d047e7cb
atapasha <atapashazade@gmail.com>
2026-06-25 13:46:08 +0330

    schema and validation added

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UserModule } from './user/user.module';
   
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/27017/ecommerce'), UserModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/ecommerce'), UserModule],
   controllers: [],
   providers: [],
 })
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -2,10 +2,10 @@ import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { User, UserSchema } from './user.schema';
+import { User, userSchema } from './user.schema';
 
 @Module({
-    imports: [MongooseModule.forFeature([{ name: User.name, schema:UserSchema }])],
+    imports: [MongooseModule.forFeature([{ name: User.name, schema:userSchema }])],
   controllers: [UserController],
   providers: [UserService],
 })
